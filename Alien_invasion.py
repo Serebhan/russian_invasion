127fed24430513aca22866a2ca28a7f37820e251
Serhio <welderssb@gmail.com>
2022-04-22 09:42:52 +0300

    Moved to left and right

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -13,18 +13,35 @@ class AlienInvasion:
 			(self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alein invasion")
 		self.ship=Ship(self)
+		
 
 	def run_game(self):
 		"""starting main cycles from game"""
 		while True:
 			self._chek_events()
 			self._update_screen()
+			self.ship.update()
 			
 	def _chek_events (self):
 		# lookin for keys or mous anyone
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				sys.exit()
+			elif event.type==pygame.KEYDOWN:
+				if event.key==pygame.K_RIGHT:
+					# flay to right side
+					self.ship.moving_right=True
+				elif event.key==pygame.K_LEFT:
+					# flay to left side
+					self.ship.moving_left=True
+			elif event.type==pygame.KEYUP:
+				if event.key==pygame.K_RIGHT:
+					self.ship.moving_right=False
+					self.ship.acceleration=0
+				elif event.key==pygame.K_LEFT:
+					self.ship.moving_left=False
+					self.ship.acceleration=0
+							
 
 	def _update_screen (self):
 		"""updating screen"""
@@ -34,7 +51,9 @@ class AlienInvasion:
 		#exponiren last painting screen
 		pygame.display.flip()
 
+	
 
+ 
 
 
 if __name__=='__main__':
--- ship.py
+++ ship.py
@@ -1,8 +1,10 @@
 import pygame
 
+
 class Ship:
 	"""Driving Ship class"""
 	def __init__(self, ai_game):
+		self.settings=ai_game.settings
 		"""initialize ship, and position"""
 		self.screen=ai_game.screen
 		self.screen_rect=ai_game.screen.get_rect()
@@ -10,6 +12,21 @@ class Ship:
 		self.image=pygame.image.load('images/ship.bmp')
 		self.rect=self.image.get_rect()
 		self.rect.midbottom = self.screen_rect.midbottom
+		#Індикатори руху:
+		self.moving_right=False
+		self.moving_left=False
+		self.acceleration=0
+		self.x=float(self.rect.x)
+
 
 	def blitme (self):
 		self.screen.blit(self.image, self.rect)
+
+	def update(self):
+		#Оновити позицію корабля якщо натиснуті клавіші вправо або вліво
+		if self.moving_right:
+			self.rect.x+=1+(self.acceleration)//1
+			self.acceleration+=self.settings.acceleration_ship
+		if self.moving_left:
+			self.rect.x-=1+(self.acceleration)//1
+			self.acceleration+=self.settings.acceleration_ship
